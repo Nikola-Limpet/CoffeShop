2ddd88f52bc2435e33c506da9209a56184b99e80
Nikola Limpet <2023465lim@aupp.edu.kh>
2024-08-21 09:13:21 +0700

    add proxy to the store

--- services/Store.js
+++ services/Store.js
@@ -3,5 +3,19 @@ const Store = {
   cart: [],
 }
 
+const proxiedStore = new Proxy(Store, {
+  set: (target, property, value) => {
+    // the set trap intercepts the assignment of a property on the target
+    // in this case the target is the Store object and property is the property being set
+    // and value is the value being assigned to the property
+    target[property] = value;
+    if (property === "menu") {
+      // Dispatch a custom event to notify the app that the menu has been updated
+      window.dispatchEvent(new CustomEvent('menu-updated'));
+    }
 
-export default Store;
+    
+  }
+});
+
+export default proxiedStore;
